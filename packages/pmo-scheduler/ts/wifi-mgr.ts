d02c2266e515f07a9bed3239b41c6dbdcc04b9a1
Liu Jing <jing.liu@bkjk.com>
2020-06-04 16:13:37 +0800

    wifi mangement with automatically turn on and send request to activate network

--- packages/pmo-scheduler/ts/wifi-mgr.ts
+++ packages/pmo-scheduler/ts/wifi-mgr.ts
@@ -1,6 +1,6 @@
 import {CronJob} from 'cron';
 import {spawn} from 'dr-comp-package/wfh/dist/process-utils';
-
+import axios from 'axios';
 
 /**
  * https://www.easycron.com/faq/What-cron-expression-does-easycron-support
@@ -10,12 +10,24 @@ export function turnOff() {
   const sec = Math.ceil(Math.random() * 60);
   const min = 53 + Math.ceil(Math.random() * 15);
 
+  const startSec = Math.ceil(Math.random() * 60);
+  const startMin = 55 + Math.ceil(Math.random() * 5);
+
   // tslint:disable-next-line: no-console
-  console.log(`Will turn off on 19:${min}:${sec}`);
+  console.log(`Will turn off at 19:${min}:${sec} and\n` +
+  `Turn on at 9:${startMin}:${startSec}`);
   new CronJob(`${sec} ${min} 19 * * 1,2,3,4,5`, () => {
     // tslint:disable-next-line: no-console
-    console.log('You will see this message every second');
+    console.log('Turning off');
     spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'off');
+  }).start();
+
+  new CronJob(`${startSec} ${startMin} 9 * * 1,2,3,4,5`, async () => {
+    // tslint:disable-next-line: no-console
+    console.log('Turning on');
+    await spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'off').promise;
+    await new Promise(resolve => setTimeout(resolve, 15000));
+    axios.get('https://www.baidu.com');
   }).start();
 }
 
