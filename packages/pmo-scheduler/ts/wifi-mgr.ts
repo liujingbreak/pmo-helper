6428093b1c1f01b370e87b8f30dd062d9a36d5d7
Liu Jing <jing.liu@bkjk.com>
2020-06-04 19:40:43 +0800

    bug fixes

--- packages/pmo-scheduler/ts/wifi-mgr.ts
+++ packages/pmo-scheduler/ts/wifi-mgr.ts
@@ -8,29 +8,39 @@ import axios from 'axios';
  */
 export function turnOff() {
   const sec = Math.ceil(Math.random() * 60);
-  const min = 53 + Math.ceil(Math.random() * 15);
+  let min = 55 + Math.ceil(Math.random() * 15);
+  let hour = 19;
+  if (min >= 60) {
+    min = min - 60;
+    hour++;
+  }
 
   const startSec = Math.ceil(Math.random() * 60);
-  const startMin = 55 + Math.ceil(Math.random() * 5);
+  let startMin = 55 + Math.ceil(Math.random() * 5);
+  let startHour = 9;
+  if (startMin >= 60) {
+    startMin = startMin - 60;
+    startHour++;
+  }
 
   // tslint:disable-next-line: no-console
-  console.log(`Will turn off at 19:${min}:${sec} and\n` +
-  `Turn on at 9:${startMin}:${startSec}`);
-  new CronJob(`${sec} ${min} 19 * * 1,2,3,4,5`, () => {
+  console.log(`Will turn off at ${hour}:${min}:${sec} and\n` +
+  `Turn on at ${startHour}:${startMin}:${startSec}`);
+  new CronJob(`${sec} ${min} ${hour} * * 1,2,3,4,5`, () => {
     // tslint:disable-next-line: no-console
     console.log('Turning off');
     spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'off');
   }).start();
 
-  new CronJob(`${startSec} ${startMin} 9 * * 1,2,3,4,5`, async () => {
+  new CronJob(`${startSec} ${startMin} ${startHour} * * 1,2,3,4,5`, async () => {
     // tslint:disable-next-line: no-console
     console.log('Turning on');
-    await spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'off').promise;
+    await spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'on').promise;
     await new Promise(resolve => setTimeout(resolve, 15000));
     axios.get('https://www.baidu.com');
   }).start();
 }
 
 export function turnOn() {
-  spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'On');
+  spawn('networksetup', '-setnetworkserviceenabled', 'Wi-Fi', 'on');
 }
